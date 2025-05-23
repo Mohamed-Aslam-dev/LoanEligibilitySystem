8cfdbece39488d3a936a5471add94d755cfe4213
Mohamed-Aslam-dev <aslam93615@gmail.com>
2025-05-23 16:33:32 +0530

    commi to packages

--- loan_eligibility_system/src/main/java/com/loan_eligibility_system/service/LoanApplyService.java
+++ loan_eligibility_system/src/main/java/com/loan_eligibility_system/service/LoanApplyService.java
@@ -1,5 +1,13 @@
 package com.loan_eligibility_system.service;
 
-public class LoanApplyService {
+import com.loan_eligibility_system.service_repository.LoanApplyServiceRepository;
+
+public class LoanApplyService implements LoanApplyServiceRepository{
+
+	
+	public String checkLoanEligibility() {
+		
+		return null;
+	}
 
 }
