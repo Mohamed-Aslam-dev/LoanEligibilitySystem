0b1fb1c48cc19dfac07fd3ea26ccc2e3fc230690
Mohamed-Aslam-dev <aslam93615@gmail.com>
2025-05-26 12:47:28 +0530

    commit to apply repo

--- loan_eligibility_system/src/main/java/com/loan_eligibility_system/repository/LoanApplyRepository.java
+++ loan_eligibility_system/src/main/java/com/loan_eligibility_system/repository/LoanApplyRepository.java
@@ -1,9 +1,11 @@
 package com.loan_eligibility_system.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.loan_eligibility_system.beans.LoanApplyPersonalData;
 
+@Repository
 public interface LoanApplyRepository extends JpaRepository<LoanApplyPersonalData, Integer> {
 	
 	
